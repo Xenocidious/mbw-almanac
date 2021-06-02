f8a4773b3cf4bcb85ff34f5c0a0c001ef9995b17
Pietto <Pieterjan.v.dijk@gmail.com>
2021-06-02 16:07:11 +0200

    V 1.0.3
    slight css changes and added images

---
+++ resources/js/animations_index.js
@@ -0,0 +1,26 @@
+console.log('reached');
+
+var images = ['NY', 'NY2', 'LO', 'UK1', 'UK2'];
+
+var lastRand = 0;
+
+setInterval(function(){
+    var rand = Math.floor(Math.random() * 4);
+
+    while(rand == lastRand){
+        rand = Math.floor(Math.random() * 4);
+    }
+    
+    index_content_1.style.backgroundImage = 'url("../resources/images/cities/' + images[rand] + '.jpg")';
+    lastRand = rand;
+    
+}, 10000);
+
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 50) {
+        $(".header").addClass("active");
+    } else {
+       $(".header").removeClass("active");
+    }
+});
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,16 +5,17 @@
         <link rel="stylesheet" href="../resources/css/app.css">
         <script src="https://kit.fontawesome.com/269ab4fa37.js" crossorigin="anonymous"></script>
         <link href="https://fonts.googleapis.com/css2?family=Cinzel&display=swap" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <title>title</title>
     </head>
     <body>
-        <header id='index_header' class='header_top'>
+        <header id='index_header' class='header_top header'>
             <div id='index_header_content'>
-                <a href='/'>item 1</a>
-                <a href='/'>item 2</a>
-                <a href='/'>item 3</a>
-                <a href='/'>item 4</a>
-                <a href='/'>item 5</a>
+                <a href='/'>about us</a>
+                <a href='/'>weather</a>
+                <a href='/'>history</a>
+                <a href='/'>photo's</a>
+                <a href='/'>account</a>
             </div>
         </header>
         <div id='index_content_1'>
@@ -23,27 +24,34 @@
 
             <div id='index_cards_wrapper'>
                 <div id='index_cards_wrapper_card_1' class='index_cards'>
-                    <h2>Yesterday</h2>
-                    <h3>20°</h3>
-                    <h4>warm</h4>
-            <h1><i class="fas fa-sun"></i></h1>
+                    <div>
+                        <h2>Yesterday</h2>
+                        <h3>20°</h3>
+                        <h4>warm</h4>
+                        <i class="fas fa-sun"></i>
+                    </div>
                 </div>
                 <div id='index_cards_wrapper_card_2' class='index_cards'>
-                    <h2>today</h2>
-                    <h3>26°</h3>
-                    <h4>extra warm</h4>
-            <h1><i class="fas fa-sun"></i></h1>
+                    <div>
+                        <h2>today</h2>
+                        <h3>26°</h3>
+                        <h4>extra warm</h4>
+                        <i class="fas fa-sun"></i>
+                    </div>
                 </div>
                 <div id='index_cards_wrapper_card_3' class='index_cards'>
-                    <h2>Tomorrow</h2>
-                    <h3>22°</h3>
-                    <h4>warm</h4>
-            <h1><i class="fas fa-sun"></i></h1>
+                    <div>
+                        <h2>Tomorrow</h2>
+                        <h3>22°</h3>
+                        <h4>warm</h4>
+                        <i class="fas fa-sun"></i>
+                    </div>
                 </div>
             </div>
         </div>
+        <div id='temp_filler'></div>
         
 
-
+    <script src='../resources/js/animations_index.js'></script>
     </body>
 </html>
