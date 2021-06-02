15bf101c66ede2052ccde59e32632795118e66c6
Pietto <Pieterjan.v.dijk@gmail.com>
2021-06-02 10:14:26 +0200

    v 1.0.0

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
+        <link rel="stylesheet" href="../resources/css/app.css">
+        <title>title</title>
+    </head>
+    <body>
+    
+    </body>
+</html>
