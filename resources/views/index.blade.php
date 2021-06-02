d15da69b80387a972dfc238b270390a29eecda2a
Pietto <Pieterjan.v.dijk@gmail.com>
2021-06-02 15:05:28 +0200

    V 1.0.2
    styling homepage in progress

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,9 +3,47 @@
     <head>
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
         <link rel="stylesheet" href="../resources/css/app.css">
+        <script src="https://kit.fontawesome.com/269ab4fa37.js" crossorigin="anonymous"></script>
+        <link href="https://fonts.googleapis.com/css2?family=Cinzel&display=swap" rel="stylesheet">
         <title>title</title>
     </head>
     <body>
-    
+        <header id='index_header' class='header_top'>
+            <div id='index_header_content'>
+                <a href='/'>item 1</a>
+                <a href='/'>item 2</a>
+                <a href='/'>item 3</a>
+                <a href='/'>item 4</a>
+                <a href='/'>item 5</a>
+            </div>
+        </header>
+        <div id='index_content_1'>
+            <h1>26 °C</h1>
+            <h1><i class="fas fa-sun"></i></h1>
+
+            <div id='index_cards_wrapper'>
+                <div id='index_cards_wrapper_card_1' class='index_cards'>
+                    <h2>Yesterday</h2>
+                    <h3>20°</h3>
+                    <h4>warm</h4>
+            <h1><i class="fas fa-sun"></i></h1>
+                </div>
+                <div id='index_cards_wrapper_card_2' class='index_cards'>
+                    <h2>today</h2>
+                    <h3>26°</h3>
+                    <h4>extra warm</h4>
+            <h1><i class="fas fa-sun"></i></h1>
+                </div>
+                <div id='index_cards_wrapper_card_3' class='index_cards'>
+                    <h2>Tomorrow</h2>
+                    <h3>22°</h3>
+                    <h4>warm</h4>
+            <h1><i class="fas fa-sun"></i></h1>
+                </div>
+            </div>
+        </div>
+        
+
+
     </body>
 </html>
