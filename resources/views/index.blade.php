dd6301672c61ccc3cc515421be7753fa1868cf04
Unknown <99048479@mydavinci.nl>
2021-06-03 03:10:29 +0200

    V 1.1.0
    Added the option to login

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
+
+
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link rel="stylesheet" href="../resources/css/app.css">
         <script src="https://kit.fontawesome.com/269ab4fa37.js" crossorigin="anonymous"></script>
         <link href="https://fonts.googleapis.com/css2?family=Cinzel&display=swap" rel="stylesheet">
@@ -15,7 +17,36 @@
                 <a href='/'>weather</a>
                 <a href='/'>history</a>
                 <a href='/'>photo's</a>
-                <a href='/'>account</a>
+                @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+                                    <a href="">Profile</a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+
             </div>
         </header>
         <div id='index_content_1'>
@@ -50,7 +81,7 @@
             </div>
         </div>
         <div id='temp_filler'></div>
-        
+
 
     <script src='../resources/js/animations_index.js'></script>
     </body>
