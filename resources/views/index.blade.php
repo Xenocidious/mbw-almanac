fa359645286c5b9606474e148cfdacf0a7ea134f
Unknown <99048479@mydavinci.nl>
2021-06-07 09:55:30 +0200

    V 1.0.2
    Added links to login

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link rel="stylesheet" href="../resources/css/animations.css">
         <link rel="stylesheet" href="../resources/css/app.css">
         <script src="https://kit.fontawesome.com/269ab4fa37.js" crossorigin="anonymous"></script>
@@ -19,7 +19,35 @@
                 <a href='/'>history</a>
                 <a href='/'>photo's</a>
                 <a href='/'>contact</a>
-                <a href='/'>account</a>
+                @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+                @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                @endif
+            @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+                        <a href="">Profile</a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endguest
             </div>
         </header>
         <div id='index_content_1'>
@@ -126,7 +154,7 @@
                             <a href="#">
                                 <span></span><span></span><span></span><span></span>
                                 <span class="fa fa-facebook"></span>
-                            </a> 
+                            </a>
                         </li>
                         <p>follow us on facebook</p>
                     </ul>
@@ -140,7 +168,7 @@
                             <a href="#">
                                 <span></span><span></span><span></span><span></span>
                                 <span class="fa fa-twitter"></span>
-                            </a> 
+                            </a>
                         </li>
                         <p>follow us on <a>twitter</a></p>
                     </ul>
@@ -155,7 +183,7 @@
                                 <span></span><span></span><span></span><span></span>
                                 <span class="fa fa-instagram"></span>
                                 <span class="fa fa-instagram insta_button"></span>
-                            </a> 
+                            </a>
                         </li>
                         <p id='footer_p'>follow us on instagram</p>
                     </ul>
@@ -169,7 +197,7 @@
                             <a href="#">
                                 <span></span><span></span><span></span><span></span>
                                 <span class="fa fa-linkedin"></span>
-                            </a> 
+                            </a>
                         </li>
                         <p>follow us on linkedin</p>
                     </ul>
