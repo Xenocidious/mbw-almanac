c5ea2a6e1548d71a6e995efb36afdeadff7f1a9a
Aya Mohammad <ayoshhhh.hadola@gmail.com>
2021-08-17 00:53:40 +0200

    layout toepassing op blade files voor DRY princiepe en CSS in resources gecompiled met npm vanuit scss zodat ze in public folder komen

--- resources/views/light-theme/layouts/app.blade.php
+++ resources/views/light-theme/layouts/app.blade.php
@@ -13,7 +13,6 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="https://kit.fontawesome.com/269ab4fa37.js" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="{{ asset('js/app.js') }}"></script>
     <script src="https://kit.fontawesome.com/269ab4fa37.js" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
@@ -24,7 +23,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    {{--    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">--}}
     <link rel="stylesheet" href="../resources/css/animations.css">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
 
@@ -32,66 +31,66 @@
 <body>
 <div id="app">
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item"><a class="nav-link" href="index">home</a></li>
+                <li class="nav-item"><a class="nav-link" href="/">about us</a></li>
+                <li class="nav-item"><a class="nav-link" href="statistics">weather</a></li>
+                <li class="nav-item"><a class="nav-link" href="/">history</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route("photohub") }}">photo's</a></li>
+            </ul>
 
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    @guest
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    @if (Route::has('register'))
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                         </li>
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
+                            <a href="{{ route('accounts.index') }}"
+                               class="dropdown-item">{{ __('Profile') }}
                             </a>
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <div class="dropdown-divider"></div>
 
-                                <a href="{{ route('accounts.index') }}"
-                                   class="dropdown-item">{{ __('Profile') }}
-                                </a>
-
-                                <div class="dropdown-divider"></div>
-
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
+                                {{ __('Logout') }}
+                            </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
-            </div>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
         </div>
     </nav>
+
     <main class="py-4">
         @if(session()->has('success'))
             <div class="alert alert-success">
@@ -106,7 +105,96 @@
 
         @yield('content')
     </main>
+
+    <footer>
+        <div class='footer_left_items'>
+            <a>Frequently Asked Questions</a><br>
+            <hr style="width:50%;text-align:left;margin-left:0">
+        </div>
+        <div class='footer_left_items'>
+            <a>Contact</a><br>
+            <hr style="width:50%;text-align:left;margin-left:0">
+        </div>
+        <div class='footer_left_items'>
+            <a>Lorem Ipsum</a><br>
+            <hr style="width:50%;text-align:left;margin-left:0">
+        </div>
+        <div class='footer_left_items'>
+            <a>Lorem Ipsum</a><br>
+            <hr style="width:50%;text-align:left;margin-left:0">
+        </div>
+        <div class='footer_left_items'>
+            <a>Lorem Ipsum</a><br>
+            <hr style="width:50%;text-align:left;margin-left:0">
+        </div>
+
+        <div class='footer_right_items'>
+                <span id='social_media_wrapper'>
+                    <ul>
+                        <li id='facebook_link'>
+                            <a href="#">
+                                <span></span><span></span><span></span><span></span>
+                                <span class="fa fa-facebook"></span>
+                            </a>
+                        </li>
+                        <p>follow us on facebook</p>
+                    </ul>
+                    <hr style="width:50%;text-align:left;margin-left:2.5vw">
+                </span>
+        </div>
+        <div class='footer_right_items'>
+                <span id='social_media_wrapper'>
+                    <ul>
+                        <li id='twitter_link'>
+                            <a href="#">
+                                <span></span><span></span><span></span><span></span>
+                                <span class="fa fa-twitter"></span>
+                            </a>
+                        </li>
+                        <p>follow us on <a>twitter</a></p>
+                    </ul>
+                    <hr style="width:50%;text-align:left;margin-left:2.5vw">
+                </span>
+        </div>
+        <div class='footer_right_items'>
+                <span id='social_media_wrapper'>
+                    <ul>
+                        <li id='instagram_link'>
+                            <a href="#">
+                                <span></span><span></span><span></span><span></span>
+                                <span class="fa fa-instagram"></span>
+                                <span class="fa fa-instagram insta_button"></span>
+                            </a>
+                        </li>
+                        <p id='footer_p'>follow us on instagram</p>
+                    </ul>
+                    <hr style="width:50%;text-align:left;margin-left:2.5vw">
+                </span>
+        </div>
+        <div class='footer_right_items'>
+                <span id='social_media_wrapper'>
+                    <ul>
+                        <li id='linkedin_link'>
+                            <a href="#">
+                                <span></span><span></span><span></span><span></span>
+                                <span class="fa fa-linkedin"></span>
+                            </a>
+                        </li>
+                        <p>follow us on linkedin</p>
+                    </ul>
+                    <hr style="width:50%;text-align:left;margin-left:2.5vw">
+                </span>
+        </div>
+
+        <div class='footer_copyright'>
+            <p>copyright project almanac Aya, Mert en Pieterjan: ©2021 - <?= date("Y"); ?></p>
+        </div>
+    </footer>
 </div>
+
+@section('body-scripts')
+    {{-- Defineer alle scripts die op het einde van de pagina ingeladen moeten worden voor Alle paginas. --}}
+@show
 
 </body>
 </html>
