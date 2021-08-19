e652fac1535032d45ed240061ecd3f9850bc043a
Aya Mohammad <ayoshhhh.hadola@gmail.com>
2021-08-20 00:49:55 +0200

    opschonen light-theme, en violet theme aangmaakt

--- app/Comment.php
+++ app/Comment.php
@@ -13,4 +13,9 @@ class Comment extends Model
     protected $table = 'images_comments';
     protected $fillable = ["image_id", "user_name", "created_at", "updated_at", "comment", "user_id"];
 
+    public function image()
+    {
+        return $this->belongsTo(Image::class);
+    }
+
 }
