ed60f517357df30f106a4cdc23fa18a8faf82a0f
Aya Mohammad <ayoshhhh.hadola@gmail.com>
2021-09-05 23:58:56 +0200

    clear toegevoegd bij weersvoorspelling icoontjes

--- resources/views/light-theme/weather/forecast.blade.php
+++ resources/views/light-theme/weather/forecast.blade.php
@@ -52,6 +52,7 @@
                                 <strong class="my-2">
                                     @switch(true)
                                         @case(stripos($forecast['days'][$i]['description'], 'sun') !== false)
+                                        @case(stripos($forecast['days'][$i]['description'], 'clear') !== false)
                                         <i class="fas fa-sun fa-5x"></i>
                                         @break
                                         @case(stripos($forecast['days'][$i]['description'], 'rain') !== false)
