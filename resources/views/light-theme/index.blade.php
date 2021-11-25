2efbbef05d1fdfc45541d16477369cb53446be79
Pieterjan van Dijk <Pieterjan.v.dijk@gmail.com>
2021-11-25 10:02:41 +0100

    introduced a map of the netherlands in public/dist/svg as netherlands.svg

--- resources/views/light-theme/index.blade.php
+++ resources/views/light-theme/index.blade.php
@@ -212,6 +212,36 @@
             </div>
             <!-- /.card -->
 
+        <!-- Main row -->
+        <div class="row">
+          <!-- Left col -->
+          <section class="col-lg-7 connectedSortable">
+            <!-- Custom tabs (Charts with tabs)-->
+            <div class="card ">
+              <div class="card-header">
+                <h3 class="card-title">
+                  <i class="fas fa-chart-bar"></i>
+                  monthly Temperature
+                </h3>
+                <div class="card-tools">
+                </div>
+              </div><!-- /.card-header -->
+              <div class="card-body">
+                <div class="tab-content p-0">
+                  <!-- Morris chart - Sales -->
+                  <div class="chart tab-pane active" id="revenue-chart"
+                       style="position: relative; height: 300px;">
+                      <canvas id="revenue-chart-canvas" height="300" style="height: 300px;"></canvas>
+                   </div>
+                  <div class="chart tab-pane" id="sales-chart" style="position: relative; height: 300px;">
+                    <canvas id="sales-chart-canvas" height="300" style="height: 300px;"></canvas>
+                  </div>
+                </div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+
             <!-- TO DO List -->
             <div class="card">
               <div class="card-header">
